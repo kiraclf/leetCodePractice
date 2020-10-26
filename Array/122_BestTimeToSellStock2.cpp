3367a1042e9cf3bc38727fcfc146debafbe84385
kiraclf <kiraclf@outlook.com>
2020-10-27 00:57:59 +0800

    122 BestTimeToBuyStock2

---
+++ Array/122_BestTimeToSellStock2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1) return 0;
+        int maxProfit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] >= prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxProfit;
+    }
+};
